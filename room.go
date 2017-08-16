b4dae97800d2b49321f7ab57f3d723006bbb8061
letsget23 <letsget23@gmail.com>
2017-08-17 01:13:24 +0900

    기본적인 입/출력 클라이언트 채널 구성

---
+++ room.go
@@ -0,0 +1,24 @@
+package main
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				client.send <- msg
+			}
+		}
+	}
+}
