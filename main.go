3cc46c5c881621d8936bf5804fb4e16195b49eac
letsget23 <letsget23@gmail.com>
2017-09-02 16:45:43 +0900

    로그아웃 기능 구현

--- main.go
+++ main.go
@@ -53,6 +53,16 @@ func main() {
 	http.Handle("/room", r)
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header().Set("Location", "/chat")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 
 	go r.run()
 
