ff4ef8382135fcaa4254113c01473853002ecff9
letsget23 <letsget23@gmail.com>
2017-09-02 17:57:29 +0900

    userid를 추가하고 Gravatar의 구현을 userid를 이용하도록 수정

--- auth.go
+++ auth.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
@@ -69,11 +71,17 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("Error when trying to get user from %s:%s", provider, err), http.StatusInternalServerError)
 			return
 		}
+
+		m := md5.New()
+		io.WriteString(m, strings.ToLower(user.Email()))
+		userID := fmt.Sprintf("%x", m.Sum(nil))
 		authCookieValue := objx.New(map[string]interface{}{
+			"userid":     userID,
 			"name":       user.Name(),
 			"avatar_url": user.AvatarURL(),
 			"email":      user.Email(),
 		}).MustBase64()
+
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
 			Value: authCookieValue,
--- avatar.go
+++ avatar.go
@@ -1,12 +1,6 @@
 package main
 
-import (
-	"crypto/md5"
-	"errors"
-	"fmt"
-	"io"
-	"strings"
-)
+import "errors"
 
 // ErrNoAvatarURL은 Avatar 인스턴스가 URL을 제공할 수 없을 때 제공되는 에러
 var ErrNoAvatarURL = errors.New("chat: avatar URL을 가져올 수 없습니다")
@@ -36,11 +30,9 @@ type GravatarAvatar struct{}
 var UseGravatar GravatarAvatar
 
 func (GravatarAvatar) GetAvatarURL(c *client) (string, error) {
-	if email, ok := c.userData["email"]; ok {
-		if emailStr, ok := email.(string); ok {
-			m := md5.New()
-			io.WriteString(m, strings.ToLower(emailStr))
-			return fmt.Sprintf("//www.gravatar.com/avatar/%x", m.Sum(nil)), nil
+	if userid, ok := c.userData["userid"]; ok {
+		if useridStr, ok := userid.(string); ok {
+			return "//www.gravatar.com/avatar/" + useridStr, nil
 		}
 	}
 	return "", ErrNoAvatarURL
